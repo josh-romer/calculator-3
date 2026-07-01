cebb6b5bc0cb4ecfaa297b3f864d9686307483be
Josh Romer <jromer94@gmail.com>
2026-07-01 16:03:59 +0000

    removed flask behind proxy from app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm
-from flask_behind_proxy import FlaskBehindProxy
 
 app = Flask(__name__)
-proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = 'c29bcfa698752666def85f68880d22d8'
 
 @app.route("/")
